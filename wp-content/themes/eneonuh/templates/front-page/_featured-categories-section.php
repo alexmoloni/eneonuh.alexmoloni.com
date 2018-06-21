c0f2823d18654010e28af8db07c02e2e9c6680e5
a-moloniewicz-aasa <aleksander@direktpoint.pl>
2018-06-21 11:11:59 +0200

    set up including boxes from centralised file so can reference it in both desktop and mobile versions

--- wp-content/themes/eneonuh/templates/front-page/_featured-categories-section.php
+++ wp-content/themes/eneonuh/templates/front-page/_featured-categories-section.php
@@ -5,92 +5,37 @@
  * Date: 18-Jun-18
  * Time: 16:37
  */
+
+require_once( 'featured-categories-boxes.php' )
 ?>
 
 <section id="featured-categories">
-    <div class="content">
+    <div class="content -desktop">
         <div class="col-left col-5">
-            <div class="box">
-                <img class="placeholder" src="images/prod/front-page/placholder.png" alt="">
-                <img src="images/prod/front-page/sukienki-etniczne.png" alt="">
-                <div class="txt-and-btn">
-                    <div class="txt">
-                        Sukienki<br>
-                        <strong>Etniczne</strong>
-                    </div>
-                    <div class="btn-pink">
-                        zobacz
-                    </div>
-                </div>
-
-            </div>
+	        <?= $boxes[1] ?>
         </div>
         <div class="col-right col-7">
             <div class="row row-top">
                 <div class="col-left">
-                    <div class="box">
-                        <img src="images/prod/front-page/bizuteria-compressor.png" alt="Biżuteria">
-                        <div class="txt-and-btn">
-                            <div class="txt">
-                                <strong>Biżuteria</strong>
-                            </div>
-                            <div class="btn-pink">
-                                zobacz
-                            </div>
-                        </div>
-
-                    </div>
+					<?= $boxes[2] ?>
                 </div>
                 <div class="col-right">
-                    <div class="box">
-                        <img src="images/prod/front-page/odziez-meska-compressor.png" alt="Odzież Męska">
-                        <div class="txt-and-btn">
-                            <div class="txt">
-                                <div class="txt">
-                                    Odzież<br>
-                                    <strong>Męska</strong>
-                                </div>
-                            </div>
-                            <div class="btn-pink">
-                                zobacz
-                            </div>
-                        </div>
-
-                    </div>
+					<?= $boxes[3] ?>
                 </div>
             </div>
             <div class="row row-bottom">
                 <div class="col-left">
-                    <div class="box">
-                        <img src="images/prod/front-page/akcesoria-etniczne-compressor.png" alt="Etniczne Akcesoria">
-                        <div class="txt-and-btn">
-                            <div class="txt">
-                                Etniczne<br>
-                                <strong>Akcesoria</strong>
-                            </div>
-                            <div class="btn-pink">
-                                zobacz
-                            </div>
-                        </div>
-
-                    </div>
+					<?= $boxes[4] ?>
                 </div>
                 <div class="col-right">
-                    <div class="box">
-                        <img src="images/prod/front-page/buty-compressor.png" alt="Buty">
-                        <div class="txt-and-btn">
-                            <div class="txt">
-                                <strong>Buty
-                                </strong>
-                            </div>
-                            <div class="btn-pink">
-                                zobacz
-                            </div>
-                        </div>
-
-                    </div>
+					<?= $boxes[5] ?>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="content content__mobile">
+<div class="featured-categories-slider">
+
+</div>
     </div>
 </section>
---
+++ wp-content/themes/eneonuh/templates/front-page/featured-categories-boxes.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aleksander
+ * Date: 21-Jun-18
+ * Time: 10:55
+ */
+
+$boxes = [
+	'1' => '            <div class="box">
+                <img class="placeholder" src="images/prod/front-page/placholder.png" alt="">
+                <img src="images/prod/front-page/sukienki-etniczne.png" alt="">
+                <div class="txt-and-btn">
+                    <div class="txt">
+                        Sukienki<br>
+                        <strong>Etniczne</strong>
+                    </div>
+                    <div class="btn-pink">
+                        zobacz
+                    </div>
+                </div>
+
+            </div>
+',
+	'2' => '                    <div class="box">
+                        <img src="images/prod/front-page/bizuteria-compressor.png" alt="Biżuteria">
+                        <div class="txt-and-btn">
+                            <div class="txt">
+                                <strong>Biżuteria</strong>
+                            </div>
+                            <div class="btn-pink">
+                                zobacz
+                            </div>
+                        </div>
+
+                    </div>
+',
+	'3' => '                    <div class="box">
+                        <img src="images/prod/front-page/odziez-meska-compressor.png" alt="Odzież Męska">
+                        <div class="txt-and-btn">
+                            <div class="txt">
+                                <div class="txt">
+                                    Odzież<br>
+                                    <strong>Męska</strong>
+                                </div>
+                            </div>
+                            <div class="btn-pink">
+                                zobacz
+                            </div>
+                        </div>
+
+                    </div>
+',
+	'4' => '                    <div class="box">
+                        <img src="images/prod/front-page/akcesoria-etniczne-compressor.png" alt="Etniczne Akcesoria">
+                        <div class="txt-and-btn">
+                            <div class="txt">
+                                Etniczne<br>
+                                <strong>Akcesoria</strong>
+                            </div>
+                            <div class="btn-pink">
+                                zobacz
+                            </div>
+                        </div>
+
+                    </div>
+',
+	'5' => '                    <div class="box">
+                        <img src="images/prod/front-page/buty-compressor.png" alt="Buty">
+                        <div class="txt-and-btn">
+                            <div class="txt">
+                                <strong>Buty
+                                </strong>
+                            </div>
+                            <div class="btn-pink">
+                                zobacz
+                            </div>
+                        </div>
+
+                    </div>
+'
+];
+
