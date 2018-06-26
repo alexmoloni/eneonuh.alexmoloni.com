dc37992645c27a7090a74d255ab71cf088b9d90b
a-moloniewicz-aasa <aleksander@direktpoint.pl>
2018-06-26 22:37:40 +0200

    before experimanting with woocommerce pages

--- wp-content/themes/eneonuh/inc/enqueue.php
+++ wp-content/themes/eneonuh/inc/enqueue.php
@@ -8,6 +8,19 @@
 // include custom jQuery
 function enqueueJsAndCss() {
 
+	//load woocommerce after my own styles
+	//first need to dequeue it
+	wp_dequeue_style( 'woocommerce-general' );
+	wp_dequeue_style( 'woocommerce-layout' );
+	wp_dequeue_style( 'woocommerce-smallscreen' );
+
+	wp_enqueue_style( 'reset', get_template_directory_uri() .'/assets/css/prod/reset.css' );
+
+	//enqueue woocommerce after my own css
+	wp_enqueue_style( 'woocommerce-general' );
+	wp_enqueue_style( 'woocommerce-layout' );
+	wp_enqueue_style( 'woocommerce-smallscreen' );
+
 	//css gets placed in head
 	wp_enqueue_style( 'main', get_template_directory_uri() .'/assets/css/dev/main.css' );
 
@@ -21,3 +34,24 @@ function enqueueJsAndCss() {
 
 }
 add_action('wp_enqueue_scripts', 'enqueueJsAndCss');
+
+
+//Dont load woocommerce styles nor scripts on non-woocommerce pages
+//Save bandwidth
+function remove_woocommerce_styles_scripts() {
+	if ( function_exists( 'is_woocommerce' ) ) {
+		if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() ) {
+			# The styles
+			wp_dequeue_style( 'woocommerce-general' );
+			wp_dequeue_style( 'woocommerce-layout' );
+			wp_dequeue_style( 'woocommerce-smallscreen' );
+
+			# The scripts
+			wp_dequeue_script( 'wc-add-to-cart' );
+			wp_dequeue_script( 'wc-cart-fragments' );
+			wp_dequeue_script( 'woocommerce' );
+		}
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'remove_woocommerce_styles_scripts', 901 );
--- wp-content/themes/eneonuh/page.php
+++ wp-content/themes/eneonuh/page.php
@@ -2,12 +2,14 @@
 get_header();
 ?>
 <section id="main">
-	<?php while ( have_posts() ) {
-		the_post(); ?>
-        <h2>This is a page</h2>
-        <h2><?php the_title() ?></h2>
-        <p><?php the_content( 'more', true ) ?></p>
-	<?php } ?>
+    <div class="content">
+	    <?php while ( have_posts() ) {
+		    the_post(); ?>
+            <h2><?php the_title() ?></h2>
+            <div class="woocommerce-wrap"><?php the_content( 'more', true ) ?></div>
+	    <?php } ?>
+    </div>
+
 </section>
 <?php get_footer();
 ?>
